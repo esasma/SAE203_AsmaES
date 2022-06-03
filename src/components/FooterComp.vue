58c8ced4f3d13a026b35d1f575dbe53314144d9a
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2022-06-03 15:10:09 +0200

    modificationfooter

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,10 +1,17 @@
 <template>
   <footer class="flex flex-col justify-between bg-gray-100 md:h-52">
     <div class="relative mt-12 flex flex-row items-center justify-between p-6">
-      <LogoBase class="w-36" />
-      <router-link to="/mentions" class="font-barlow text-lg font-bold text-black">Mentions légales</router-link>
+      <LogoBase class="ml-1 mr-2 w-36" />
+      <router-link to="/mentions" class="text-semibold font-barlow text-sm text-black lg:text-lg lg:font-bold"
+        >Mentions légales</router-link
+      >
       <RouterLink to="/admin">
-        <button type="submit" class="self-end bg-cyan-700 px-7 py-1 font-barlow text-lg font-bold uppercase text-gray-100">Admin</button>
+        <button
+          type="submit"
+          class="text-semibold self-end bg-cyan-700 px-4 py-2 font-barlow font-bold uppercase text-gray-100 lg:px-7 lg:py-1 lg:text-lg"
+        >
+          Admin
+        </button>
       </RouterLink>
     </div>
   </footer>
